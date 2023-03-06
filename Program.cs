0a1ad5244851a9cb4764490fed111548f35c2bad
nikitapani4 <115365241+nikitapani4@users.noreply.github.com>
2023-03-06 11:15:24 +0300

    Решение задания 10

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+Console.Write("Введите число: ");
+
+int numb = int.Parse(Console.ReadLine());
+int a;
+
+numb = numb / 10;
+a = numb % 10;
+
+Console.WriteLine ($"Вторая цифра числа numb = {a}");
